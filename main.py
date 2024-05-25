73148d5c937374cd7bb16f1fbb7783361ee5547b
anu-raj-16 <anaghag.1612@gmail.com>
2024-05-24 19:41:29 -0700

    Initial font set up

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+font = pygame.font.Font('8-bit Arcade Out.ttf', 20)
+big_font = pygame.font.Font('8-bit Arcade Out.ttf', 50)
+timer = pygame.time.Clock()
+fps = 60
