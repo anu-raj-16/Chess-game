80e16c7ee06c672dfc222318fd44805c28982215
JennWan <144317905+JennWan@users.noreply.github.com>
2024-05-24 19:52:59 -0700

    edited font name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 900
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-font = pygame.font.Font('8-bit Arcade Out.ttf', 20)
-big_font = pygame.font.Font('8-bit Arcade Out.ttf', 50)
+font = pygame.font.SysFont('fonts/8-bit Arcade Out.ttf', 20)
+big_font = pygame.font.SysFont('fonts/8-bit Arcade Out.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
