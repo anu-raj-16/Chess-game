329ca6a7a9830fc9782e4ebed0eda423169a5cb3
JennWan <144317905+JennWan@users.noreply.github.com>
2024-05-24 20:00:45 -0700

    main game loop

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ font = pygame.font.SysFont('fonts/8-bit Arcade Out.ttf', 20)
 big_font = pygame.font.SysFont('fonts/8-bit Arcade Out.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
+
+# main game loop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+
+    # event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+              run = False
+
+    pygame.display.flip()
+pygame.quit()
+            
+                
